98f05a0ec7527e994723383437c2635ec334560b
TejaswiniJB <103590546+TejaswiniJB@users.noreply.github.com>
2022-10-10 20:03:23 +0530

    Add files via upload

---
+++ Audit-Authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Shreyasee Ghosh', 'sg20', 'sasa1234', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Monika Verma', 'mv', 'mv1234', 'Project-2');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Avinash Pandey', 'ap', 'ap1234', 'Project-3');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Tejaswini JB', 'teju', 'teju1234', 'Project-4');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Shyam Sundar Jha', 'ss', 'ss1234', 'Project-5');
